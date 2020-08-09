e03d4677d8850b090746e50231428d390cf5d3cf
Vu <hoovadateam@gmail.com>
2020-08-09 16:56:12 +0000

    Add entrypoint for dockerfile Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+#if [[ -d /migrations ]]; then
+#    echo "Removing migrations folders"
+#    rm -rf /migrations
+#fi
+
+flask db migrate
+flask db upgrade
+
+gunicorn --worker-tmp-dir /dev/shm  --worker-connections 1001 --workers 4 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 wsgi:app
