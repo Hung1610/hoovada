9829056ffab97c9cb198ad385fe6ffd406440a93
Tran Ly Vu <vutransingapore@gmail.com>
2020-08-10 13:21:02 +0800

    Migration
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,9 @@ set -e
 
 #flask db init
 #flask db stamp head
-DATABASE_URL=sqlite:/// flask db migrate	
-flask db upgrade
+#flask db migrate
+
+flask db migrate	
+flask db stamp head
 
 gunicorn --worker-tmp-dir /dev/shm  --worker-connections 1001 --workers 4 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 wsgi:app
