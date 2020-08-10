e7f8253f231cc15c68dce8d553fc45d5c2551b7a
Tran Ly Vu <vutransingapore@gmail.com>
2020-08-10 17:25:23 +0800

    Add gunicorn logging conf
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 flask db migrate	
 flask db upgrade
 
-gunicorn --log-config gunicorn_logging.conf --worker-tmp-dir /dev/shm  --worker-connections 1001 --workers 4 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 wsgi:app
+gunicorn --log-config ./gunicorn_logging.conf --worker-tmp-dir /dev/shm  --worker-connections 1001 --workers 4 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 wsgi:app
