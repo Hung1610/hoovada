f5dcb5d5e1dc7f2568ddb3668216d66b38e47c81
Tran Ly Vu <vutransingapore@gmail.com>
2020-08-17 12:05:57 +0800

    Update mail password
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- app/settings/config.py
+++ app/settings/config.py
@@ -25,7 +25,7 @@ class BaseConfig:
     MAIL_USE_SSL = False
     MAIL_USERNAME =  os.environ.get('MAIL_USERNAME', 'hoovada.test@gmail.com')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME', 'hoovada.test@gmail.com') 
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'gydijkoduvmmcjjx')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'megkyvytyktpkkbo')
     ADMINS = ['admin@hoovada.com'] # list of emails to receive error reports
 
     # need to set this so that email can be sent
