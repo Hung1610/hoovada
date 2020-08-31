b1aff669d3d4238dea2c0007bc9427b26d3c43b1
Vu <hoovadateam@gmail.com>
2020-08-31 16:25:15 +0000

    Add enchant into dockerfile
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- sql/v1.0_0_db_migration.sql
+++ sql/v1.0_0_db_migration.sql
@@ -84,3 +84,10 @@ FROM
             )
         )
     )
+
+ALTER TABLE `hoovada`.`question` CHANGE COLUMN `id` `id` INT(10) NOT NULL AUTO_INCREMENT;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM question WHERE user_id NOT IN (SELECT user.id FROM user);
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM question_topic WHERE question_id NOT IN (SELECT question .id FROM question);
+UPDATE question SET is_private = False;
