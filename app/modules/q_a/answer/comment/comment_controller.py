cb72802a150b676eb60a89abe69a17fa4109fd54
Do Doan Phuoc Hung <phuochung008@gmail.com>
2020-09-28 15:45:09 +0000

    fix is_deactivated get API filtering

--- app/modules/q_a/answer/comment/comment_controller.py
+++ app/modules/q_a/answer/comment/comment_controller.py
@@ -47,7 +47,7 @@ class CommentController(Controller):
                 pass
 
         query = AnswerComment.query
-        query = query.filter(AnswerComment.user.is_deactivated != True)
+        query = query.join(User).filter(db.or_(AnswerComment.user == None, User.is_deactivated != True))
         if answer_id is not None:
             query = query.filter(AnswerComment.answer_id == answer_id)
         if user_id is not None:
