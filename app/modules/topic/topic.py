ec172ed0cc721f30d08f8d067a12fd85cf9c09b4
Do Doan Phuoc Hung <phuochung008@gmail.com>
2020-09-29 14:42:33 +0000

    Hung dev

--- app/modules/topic/topic.py
+++ app/modules/topic/topic.py
@@ -7,6 +7,7 @@ from slugify import slugify
 
 # third-party modules
 from sqlalchemy_utils import aggregated
+from sqlalchemy.sql import expression
 from sqlalchemy import event
 
 # own modules
@@ -49,7 +50,7 @@ class Topic(Model):
     is_fixed = db.Column(db.Boolean, default=False)  # is this topic fixed?
     created_date = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(255))
-    is_nsfw = db.Column(db.Boolean, default=False)  # is this topic fixed?
+    is_nsfw = db.Column(db.Boolean, server_default=expression.false())  # is this topic fixed?
     endorsed_users = db.relationship('User', secondary='topic_user_endorse', foreign_keys=[TopicUserEndorse.user_id, TopicUserEndorse.topic_id], lazy='dynamic')
     fixed_topic_articles = db.relationship("Article", cascade='all,delete-orphan')
     avatar = db.Column(db.String(255))
