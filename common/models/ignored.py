b6863c6a518455828f412c67d34e65fe1a4e5a32
Do Doan Phuoc Hung <phuochung008@gmail.com>
2021-01-08 08:57:46 +0000

    Hung dev

--- common/models/ignored.py
+++ common/models/ignored.py
@@ -4,18 +4,18 @@
 # -*- coding: utf-8 -*-
 
 # built-in modules
-import enum
-from datetime import datetime
 
 # own modules
-from common.db import db
-from common.enum import BanTypeEnum
 from common.models.model import Model
 
 __author__ = "hoovada.com team"
 __maintainer__ = "hoovada.com team"
 __email__ = "admin@hoovada.com"
 __copyright__ = "Copyright (c) 2020 - 2020 hoovada.com . All Rights Reserved."
+
+
+# Define all tables that you want Flask Migrate/Alembic to ignore
+# while creating migrations, upgrading the database
 
 
 class ApschedulerJobs(Model):
