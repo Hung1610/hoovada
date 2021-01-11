7ad0471db321e3b84f2676a4ac421ece9aa0eb5a
Do Doan Phuoc Hung <phuochung008@gmail.com>
2021-01-11 07:45:42 +0000

    Hung dev

--- common/models/article.py
+++ common/models/article.py
@@ -41,19 +41,19 @@ class Article(Model, AuditCreateMixin, AuditUpdateMixin, AnonymousMixin):
     allow_voting = db.Column(db.Boolean, default=True)
     views_count = db.Column(db.Integer, default=0)
 
-    @aggregated('votes', db.Column(db.Integer, server_default="0"))
+    @aggregated('votes', db.Column(db.Integer, server_default="0", nullable=False))
     def upvote_count(self):
         return db.func.sum(db.func.if_(db.text("vote_status = 'UPVOTED'"), 1, 0))
-    @aggregated('votes', db.Column(db.Integer, server_default="0"))
+    @aggregated('votes', db.Column(db.Integer, server_default="0", nullable=False))
     def downvote_count(self):
         return db.func.sum(db.func.if_(db.text("vote_status = 'DOWNVOTED'"), 1, 0))
-    @aggregated('article_shares', db.Column(db.Integer, server_default="0"))
+    @aggregated('article_shares', db.Column(db.Integer, server_default="0", nullable=False))
     def share_count(self):
         return db.func.count('1')
-    @aggregated('article_favorites', db.Column(db.Integer, server_default="0"))
+    @aggregated('article_favorites', db.Column(db.Integer, server_default="0", nullable=False))
     def favorite_count(self):
         return db.func.count('1')
-    @aggregated('article_comments', db.Column(db.Integer, server_default="0"))
+    @aggregated('article_comments', db.Column(db.Integer, server_default="0", nullable=False))
     def comment_count(self):
         return db.func.count('1')
 
