c2d42b1391ceb9b877289ed26e32f0b3d225d568
hoovada <vutransingapore@gmail.com>
2021-01-31 23:34:05 +0800

    Loosen sensitive check
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- common/utils/sensitive_words.py
+++ common/utils/sensitive_words.py
@@ -7,23 +7,26 @@ __email__ = "admin@hoovada.com"
 __copyright__ = "Copyright (c) 2020 - 2020 hoovada.com . All Rights Reserved."
 
 
-sensitive_words = ['lồn', 'dm', 'du me', 'đu me', 'đủ mẹ', 'đủ má', 'đéo', 'địt', 'dâm đãng', 'làm tình',
-                   'cuto', 'chat sex', 'loạn luân', 'slm', 'dkm', 'porn', 'giao lưu sex', 'nc sex', 'nc dâm',
+sensitive_words = ['dm', 'du me', 'đu me', 'đủ mẹ', 'đủ má', 'đéo', 'địt', 'dâm đãng', 'làm tình',
+                   'cuto', 'chat sex', 'loạn luân', 'dkm', 'giao lưu sex', 'nc sex', 'nc dâm',
                    'fuck ', 'chat xxx', 'sax', 'phá trinh', 'tùm gái', ' tìm trai', 'tìm bạn nữ', 'childporn', 'bắn tinh', 'việt cộng',
-                   'trai bao', 'gái bao', 'web sex', 'phim sex', 'film sex', 'child porn', 'gái dâm', 'trai dâm', 'cặc', 'vãi đái',
-                   'nứng lồn', 'some nam nữ', 'sex trẻ em', 'quay tay', 'sục', 'dâm', 'kèo nhà cái', 'video sex', 'chat nude',
+                   'web sex', 'phim sex', 'film sex', 'child porn', 'gái dâm', 'trai dâm', 'cặc', 'vãi đái',
+                   'nứng lồn', 'quay tay', 'sục', 'kèo nhà cái', 'video sex', 'chat nude',
                    'casino online', 'nung lon', 'video xxx', 'vkl', 'lập group', 'chát sex', 'chơi casino', 'ảnh sex']
 
 
 def check_sensitive(text):
     if text is None or str(text).strip().__eq__(''):
-        return False
+      return False
+
     result = False
+
     for word in sensitive_words:
         word = str(word).strip()
-        if word in text or str(word).upper() in text or str(word).lower() in text:
+        if word.lower() in text.lower():
             result = True
             break
+
     return result
 
 # if __name__ == '__main__':
