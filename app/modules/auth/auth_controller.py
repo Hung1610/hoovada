09ed8c7c77215d3ed09a6a46fe8357b94336f295
hoovada <vutransingapore@gmail.com>
2021-03-25 19:06:38 +0800

    Improve auth controller code
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- app/modules/auth/auth_controller.py
+++ app/modules/auth/auth_controller.py
@@ -765,9 +765,10 @@ class AuthController:
             return send_error(message=messages.ERR_NO_TOKEN)
         
         try:  
-            user_id, _ = decode_auth_token(auth_token=auth_token)
-            user = User.get_user_by_id(user_id)
+            #user_id, _ = decode_auth_token(auth_token=auth_token)
+            #user = User.get_user_by_id(user_id)
 
+            user = g.current_user
             if user is None:
                 return send_error(message=messages.ERR_NOT_LOGIN)
 
