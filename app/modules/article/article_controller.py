a0b80ae6355d1e88fe06cdfd835880c06a7cb35e
hoovada <vutransingapore@gmail.com>
2021-03-29 14:12:24 +0800

    Fix bugs
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- app/modules/article/article_controller.py
+++ app/modules/article/article_controller.py
@@ -163,7 +163,7 @@ class ArticleController(Controller):
         if params.get('topic_ids'):
             query = query.filter(Article.topics.any(Topic.id.in_(params.get('topic_ids'))))
         if params.get('article_ids'):
-             print(params.get('article_ids'))
+            print(params.get('article_ids'))
             query = query.filter(Article.id.in_(params.get('article_ids')))
         if params.get('draft') is not None:
             if params.get('draft'):
