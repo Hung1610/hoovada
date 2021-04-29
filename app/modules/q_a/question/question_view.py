bf85e2a7fb86375f5c4722f0e75333d836835088
hoovada <vutransingapore@gmail.com>
2021-04-29 23:48:02 +0800

    Change data params for question deleteion proposal
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- app/modules/q_a/question/question_view.py
+++ app/modules/q_a/question/question_view.py
@@ -191,9 +191,9 @@ class QuestionDeleteProposal(Resource):
     def post(self, id_or_slug):
         """ Create question delete proposal by question Id or slug"""
 
-        args = proposal_get_parser.parse_args()
+        data = api.payload
         controller = QuestionController()
-        result = controller.create_question_deletion_proposal(object_id=id_or_slug, args=args)
+        result = controller.create_question_deletion_proposal(object_id=id_or_slug, data=data)
         cache.clear_cache(get_question_proposal_key_prefix())
         return result
 
