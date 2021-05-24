e50a4460305c9c6295f205e1f0960ad7ce49db56
hoovada <vutransingapore@gmail.com>
2021-05-24 22:04:52 +0800

    Fix messages for follow APIs
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- app/modules/user/follow/follow_controller.py
+++ app/modules/user/follow/follow_controller.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # third-party modules
 import dateutil.parser
-from flask import current_app, request
+from flask import g
 from flask_restx import marshal
 from sqlalchemy import and_
 
@@ -25,6 +25,7 @@ __copyright__ = "Copyright (c) 2020 - 2020 hoovada.com . All Rights Reserved."
 
 
 class UserFollowController(Controller):
+
     def get(self, object_id, args):
         
         follower_id, from_date, to_date = None, None, None
@@ -34,12 +35,14 @@ class UserFollowController(Controller):
             except Exception as e:
                 print(e.__str__())
                 pass
+
         if 'from_date' in args:
             try:
                 from_date = dateutil.parser.isoparse(args['from_date'])
             except Exception as e:
                 print(e.__str__())
                 pass
+
         if 'to_date' in args:
             try:
                 to_date = dateutil.parser.isoparse(args['to_date'])
@@ -47,23 +50,31 @@ class UserFollowController(Controller):
                 print(e.__str__())
                 pass
 
-        query = UserFollow.query
-        if object_id is not None:
-            query = query.filter(UserFollow.followed_id == object_id)
-        if follower_id is not None:
-            query = query.filter(UserFollow.follower_id == follower_id)
-        if from_date is not None:
-            query = query.filter(UserFollow.created_date >= from_date)
-        if to_date is not None:
-            query = query.filter(UserFollow.created_date <= to_date)
-        follows = query.all()
+        try:
+            query = UserFollow.query
+            if object_id is not None:
+                query = query.filter(UserFollow.followed_id == object_id)
+                
+            if follower_id is not None:
+                query = query.filter(UserFollow.follower_id == follower_id)
 
-        return send_result(data=marshal(follows, UserFollowDto.model_response), message='Success')
+            if from_date is not None:
+                query = query.filter(UserFollow.created_date >= from_date)
+
+            if to_date is not None:
+                query = query.filter(UserFollow.created_date <= to_date)
+            follows = query.all()
+
+            return send_result(data=marshal(follows, UserFollowDto.model_response), message=messages.MSG_GET_SUCCESS)
+        
+        except Exception as e:
+            print(e.__str__())
+            return send_error(message=messages.ERR_GET_FAILED.format(e))
 
 
     def create(self, object_id):
         
-        current_user, _ = current_app.get_logged_user(request)
+        current_user = g.current_user
         data = {}
         data['follower_id'] = current_user.id
         data['followed_id'] = object_id
@@ -71,7 +82,7 @@ class UserFollowController(Controller):
             follow = UserFollow.query.filter(UserFollow.follower_id == data['follower_id'],
                                              UserFollow.followed_id == data['followed_id']).first()
             if follow is not None:
-                return send_result(message=messages.ERR_ISSUE.format('Already followed'))
+                return send_result(message=messages.ERR_ALREADY_EXISTS)
 
             follow = self._parse_follow(data=data, follow=None)
             db.session.add(follow)
@@ -88,11 +99,17 @@ class UserFollowController(Controller):
     def get_by_id(self, object_id):
         if object_id is None:
             return send_error(message=messages.ERR_PLEASE_PROVIDE.format('object_id'))
-        follow = UserFollow.query.filter_by(id=object_id).first()
-        if follow is None:
-            return send_error(message=messages.ERR_NOT_FOUND_WITH_ID.format('Follow', object_id))
-        else:
-            return send_result(data=marshal(follow, UserFollowDto.model_response), message='Success')
+
+        try:
+            follow = UserFollow.query.filter_by(id=object_id).first()
+            if follow is None:
+                return send_error(message=messages.ERR_NOT_FOUND)
+            else:
+                return send_result(data=marshal(follow, UserFollowDto.model_response), message=messages.MSG_GET_SUCCESS)
+
+        except Exception as e:
+            print(e.__str__())
+            return send_error(message=messages.ERR_GET_FAILED.format(e))
 
 
     def update(self, object_id, data):
@@ -100,20 +117,23 @@ class UserFollowController(Controller):
 
 
     def delete(self, object_id):
-        current_user, _ = current_app.get_logged_user(request)
+        current_user = g.current_user
         user_id = current_user.id
+
         try:
             follow = UserFollow.query.filter_by(followed_id=object_id, follower_id=user_id).first()
             if follow is None:
-                return send_result(message=messages.ERR_ISSUE.format('No follow entity found'))
+                return send_result(message=messages.ERR_NOT_FOUND)
 
             db.session.delete(follow)
             db.session.commit()
             return send_result(message=messages.MSG_DELETE_SUCCESS)
+
         except Exception as e:
             db.session.rollback()
             print(e.__str__())
             return send_error(message=messages.ERR_DELETE_FAILED.format(e))
+
 
     def get_top_users(self, object_id, args):
         try:
@@ -139,25 +159,29 @@ class UserFollowController(Controller):
                 .limit(limit)\
                 .all()
             results = [{'user': user, 'total_score': total_score} for user, total_score in top_users]
-            return send_result(data=marshal(results, UserFollowDto.top_user_followee_response), message='Success')
+            return send_result(data=marshal(results, UserFollowDto.top_user_followee_response), message=messages.MSG_GET_SUCCESS)
+
         except Exception as e:
             print(e.__str__())
-            return send_error(message="Get recommended users failed. Error: " + e.__str__())
+            return send_error(message=messages.ERR_DELETE_FAILED.format(e))
 
 
     def _parse_follow(self, data, follow=None):
         if follow is None:
             follow = UserFollow()
+
         if 'follower_id' in data:
             try:
                 follow.follower_id = int(data['follower_id'])
             except Exception as e:
                 print(e.__str__())
                 pass
+
         if 'followed_id' in data:
             try:
                 follow.followed_id = int(data['followed_id'])
             except Exception as e:
                 print(e.__str__())
                 pass
+
         return follow
