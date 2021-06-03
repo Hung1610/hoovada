c2afaae17a0231b1af44faf8f9327300059e3cd3
hoovada <vutransingapore@gmail.com>
2021-06-03 11:31:19 +0800

    Fix bugs
    Signed-off-by: hoovada <vutransingapore@gmail.com>

--- app/modules/q_a/question/question_controller.py
+++ app/modules/q_a/question/question_controller.py
@@ -347,8 +347,8 @@ class QuestionController(Controller):
             if question is None or question.is_deleted is True:
                 return send_error(message=messages.ERR_NOT_FOUND)
 
-            question_deletion_proposal = QuestionProposal.query.filter_by(question_id=question.id, is_parma_delete=1, is_approved=0)
-            if question_deletion_proposal is not None or len(question_deletion_proposal) > 0:
+            question_deletion_proposal = QuestionProposal.query.filter_by(question_id=question.id, is_parma_delete=1, is_approved=0).first()
+            if question_deletion_proposal is not None:
                 return send_error(message="Question deletion proposal ID {} has been sent and is pending!".format(object_id))
 
             data = {}
